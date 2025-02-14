c2e913316e23c8ff0a517908df48dba15663ff06
prabhakar mandloi <prabhakarmandloi@gmail.com>
2025-02-14 18:15:31 +0530

    Added client side validation

--- ORS_Project4/src/test/java/ArmstrongNumber.java
+++ ORS_Project4/src/test/java/ArmstrongNumber.java
@@ -1,24 +1,5 @@
 
 public class ArmstrongNumber {
 
-	public static void main(String[] args) {
-		int num = 8208;
-		
-		int temp1 = num;
-		int temp2 = 0;
-		int count = 0;
-		
-		count = String.valueOf(num).length();
-		
-		while(temp1 != 0) {
-			int r = temp1 % 10;
-			temp2 += Math.pow(r,count);
-			temp1 = temp1 / 10;
-		}
-		
-		System.out.println(" num "+ num + " " + "temp2 "+ temp2);
-			
-
-	}
-
+	
 }
